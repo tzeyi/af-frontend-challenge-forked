cb65664cea4780be12377b9f2f66c0a80777d448
tzeyi <tytiong2@gmail.com>
2025-03-19 17:15:07 -0400

    carousel history works. Now some quality of life updates

--- cats-app/src/app/page.js
+++ cats-app/src/app/page.js
@@ -3,42 +3,53 @@
 import { useState } from 'react';
 
 export default function Home() {
-  const [catImg, setCatImg] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [catImg, setCatImg] = useState([]);
+  const [newCatIndex, setNewCatIndex] = useState(0);
 
   const fetchCat = async () => {
-    setIsLoading(true);
-    
-    // Generate a unique URL with timestamp to prevent caching
+    // Generate a unique URL with timestamp 
     const timestamp = new Date().getTime();
     const catUrl = `https://cataas.com/cat?${timestamp}`;
     
-    // Update the cat image URL
-    setCatImg(catUrl);
-    setIsLoading(false);
+    setCatImg(prevImg => [...prevImg, catUrl]);
+    setNewCatIndex(newCatIndex + 1);
+
+    setTimeout(() => {
+      window.location.hash = `slide${newCatIndex}`;
+    }, 500);
+
+    console.log(catImg)
+  }
+
+  const clearCat = () => {
+    setCatImg([])
+    setNewCatIndex(0)
+    window.location.hash = `slide${newCatIndex}`
   }
 
   return (
     <>
       <main>
         <div className="mx-auto text-center">
-          <h1> Cats Frenzy!</h1>
-          <button className="btn btn-info" onClick={fetchCat}>Spawn cats</button>
+          <h1 className="p-5"> Number of cats spawned: {newCatIndex} </h1>
+          <button className="btn btn-info mr-10" onClick={fetchCat}>Spawn cats</button>
+          <button className="btn btn-soft btn-error ml-10 " onClick={clearCat}>Clear cats</button>
         </div>
 
-        <div className="mx-auto">
-          <div className="card">
-            {catImg === "" ? (
-              <h1> Click the button !</h1>
-            ) : isLoading ? (
-              <h1> Loading...</h1>
-            ) : (
-              <figure>
-                <img src={catImg} alt="Cat" />
-              </figure>
-            )}
+        <section className="w-1/2 h-1/2 mx-auto pt-15">
+          <div className="carousel relative">
+            {catImg.map((catImg, index) => (
+              <div id={`slide${index}`} key={index} className="carousel-item relative w-full">
+                <img src={catImg} className="w-max h-max object-contain" alt={index}/>
+
+                <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 flex justify-between">
+                  <a href={`#slide${index === 0 ? catImg.length - 1 : index - 1}`} className="btn btn-circle"> ❮ </a>
+                  <a href={`#slide${index === catImg.length - 1 ? 0 : index + 1}`} className="btn btn-circle"> ❯ </a>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
+        </section>
 
       </main>
     </>
